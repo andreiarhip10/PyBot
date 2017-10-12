abfbb2ce733aa37094f8564f56972b92eb14475f
sabisav <sabisav@yahoo.com>
2017-10-12 23:04:04 +0300

    user verification

--- main.py
+++ main.py
@@ -8,15 +8,17 @@ kernel = aiml.Kernel()
 if os.path.isfile("bot_brain.brn"):
     kernel.bootstrap(brainFile = "bot_brain.brn")
 else:
-    kernel.bootstrap(learnFiles = "std_startup.xml", commands = "LOAD AIML B")
+    kernel.bootstrap(learnFiles = "basic_chat.aiml")
     kernel.saveBrain("bot_brain.brn")
 
 sessionData = kernel.getSessionData(sessionId)
 
 
 occupation=0
-path = 'E:\FII\Anul 3\Inteligenta Artificiala\Laborator 1\PyBot\users.txt'
-
+path = "C:\Users\jon\Desktop\PyBot\users.txt"
+username=""
+ok=0
+exists=0
 
 while True:
     message = raw_input("\nYou: ")
@@ -25,9 +27,9 @@ while True:
         print ("Anon: Bye" )
         exit()
     elif message == "save":
-        kernel.bootstrap(learnFiles="std_startup.xml", commands="LOAD AIML B")
+        kernel.bootstrap(learnFiles="basic_chat.aiml")
         kernel.saveBrain("bot_brain.brn")
-     elif message == "hello":
+    elif message == "hello":
         bot_response = kernel.respond(message, sessionId)
         print ("Anon: " + bot_response)
     else:
@@ -51,11 +53,21 @@ while True:
                 else:
                      bot_response4 = kernel.respond(message, sessionId)
                      print ("Anon: " + bot_response4)
+                     if occupation!="" and ok==0:
+                         import csv
+                         with open(path) as inf:
+                             reader = csv.reader(inf, delimiter=" ")
+                             for line in reader:
+                                if username==line[0] and age==line[1] and occupation==line[2]:
+                                    exists=1;
+                         inf.close();
+                         if exists==0:
+                             with open(path, 'a') as file:
+                                file.write(username + ' ' + age + ' ' + occupation + '\n')
+                             ok = 1
+                         else:
+                             print("Anon: Have we talked before?")
+    #if ok==1:
+        #kernel.bootstrap(learnFiles="std_startup.xml", commands="LOAD AIML B")
+        #kernel.saveBrain("bot_brain.brn")
 
-
-        # occupation = kernel.getPredicate("occupation", sessionId)
-        # if occupation != 0:
-        #     username = kernel.getPredicate("username", sessionId)
-        #     age = kernel.getPredicate("age", sessionId)
-        #     with open(path, 'a') as file:
-        #         file.write(username+' '+age+' '+occupation+'\n')

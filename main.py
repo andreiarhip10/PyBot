34d6b54d47ffc29baaee1e464baafb84428915d0
sabisav <sabisav@yahoo.com>
2017-10-12 20:05:33 +0300

    requesting user info

--- main.py
+++ main.py
@@ -27,12 +27,35 @@ while True:
     elif message == "save":
         kernel.bootstrap(learnFiles="std_startup.xml", commands="LOAD AIML B")
         kernel.saveBrain("bot_brain.brn")
+     elif message == "hello":
+        bot_response = kernel.respond(message, sessionId)
+        print ("Anon: " + bot_response)
     else:
         bot_response = kernel.respond(message, sessionId)
-        print ("Anon: "+bot_response)
-        occupation = kernel.getPredicate("occupation", sessionId)
-        if occupation != 0:
-            username = kernel.getPredicate("username", sessionId)
+        username = kernel.getPredicate("username", sessionId)
+        if username =="":
+            bot_response = kernel.respond("name not given", sessionId)
+            print ("Anon: " + bot_response)
+        else:
+            bot_response = kernel.respond(message, sessionId)
             age = kernel.getPredicate("age", sessionId)
-            with open(path, 'a') as file:
-                file.write(username+' '+age+' '+occupation+'\n')
+            if age =="":
+                bot_response1 = kernel.respond("age not given", sessionId)
+                print ("Anon: " + bot_response + ' '+bot_response1)
+            else:
+                bot_response2 = kernel.respond(message, sessionId)
+                occupation = kernel.getPredicate("occupation", sessionId)
+                if occupation == "":
+                     bot_response3 = kernel.respond("occupation not given", sessionId)
+                     print ("Anon: " + bot_response2+ ' '+ bot_response3)
+                else:
+                     bot_response4 = kernel.respond(message, sessionId)
+                     print ("Anon: " + bot_response4)
+
+
+        # occupation = kernel.getPredicate("occupation", sessionId)
+        # if occupation != 0:
+        #     username = kernel.getPredicate("username", sessionId)
+        #     age = kernel.getPredicate("age", sessionId)
+        #     with open(path, 'a') as file:
+        #         file.write(username+' '+age+' '+occupation+'\n')
